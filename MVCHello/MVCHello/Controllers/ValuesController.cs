e18f6ec265b7f61f126a6c6457f775bda6c82fe9
Qian Zhou <qianfinland@gmail.com>
2015-07-06 22:14:17 +0300

    MVC example

--- MVCHello/MVCHello/Controllers/ValuesController.cs
+++ MVCHello/MVCHello/Controllers/ValuesController.cs
@@ -10,31 +10,44 @@ namespace MVCHello.Controllers
     [Authorize]
     public class ValuesController : ApiController
     {
+        static List<string> data = initList();
+        private static List<string> initList()
+        { 
+            var ret = new List<string>();
+            ret.Add("value1");
+            ret.Add("value2");
+            return ret;
+        }
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            //return new string[] { "value1", "value2" };
+            return data;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            //return "value";
+            return data[id];
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            data.Add(value);
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+            data[id] = value;
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            data.RemoveAt(id);
         }
     }
 }
