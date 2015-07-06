f31a32d6cb0faf97f70515f282ca942a4447b53e
Qian Zhou <qianfinland@gmail.com>
2015-07-06 13:37:23 +0300

    first controller

---
+++ FirstASPNetAPI/FirstASPNetAPI/DemoApiController.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Web.Http;
+namespace FirstASPNetAPI
+{
+    public class DemoApiController : ApiController
+    {
+        public string Get()
+        { 
+            return "Hello from API at " + DateTime.Now.ToString();
+        }
+
+    }
+}
