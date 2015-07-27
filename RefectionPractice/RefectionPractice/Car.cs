7dd18c06ea65432a66c55b8290728fff7b8ef33a
Qian Zhou <qianfinland@gmail.com>
2015-07-27 10:36:34 +0300

    Selenium with c#

---
+++ RefectionPractice/RefectionPractice/Car.cs
@@ -0,0 +1,11 @@
+﻿
+using Newtonsoft.Json;
+namespace RefectionPractice
+{
+    class Car
+    {
+        [JsonProperty(Required = Required.Always)]
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ SeleniumFirst/SeleniumFirst/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create the reference for our browser
+            IWebDriver driver = new ChromeDriver();
+
+            //navigate to Google page
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+            //find the element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            //Perform ops
+            element.SendKeys("excuteautomation");
+        }
+    }
+}
