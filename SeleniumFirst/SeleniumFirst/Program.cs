1a28b7be91bcdedd526e582672e68f4b44398144
Qian Zhou <qianfinland@gmail.com>
2015-07-30 15:42:20 +0300

    read data from excel and test from there

--- SeleniumFirst/SeleniumFirst/Program.cs
+++ SeleniumFirst/SeleniumFirst/Program.cs
@@ -25,18 +25,21 @@ namespace SeleniumFirst
         [Test]
         public void ExecuateTest()
         {
-
+            //intialize the excel sheet
+            ExcelLib.PopulateInCollection(@"C:\Users\Qian.Zhou\Documents\DataSelenium.xlsx");
             //login to application
             LoginPageObject pageLogin = new LoginPageObject();
-            EAPageObject pageEA= pageLogin.Login("bunny","guessmypassword");
-
+            //EAPageObject pageEA= pageLogin.Login("bunny","guessmypassword");
+            EAPageObject pageEA = pageLogin.Login(ExcelLib.ReadData(1, "UserName"), ExcelLib.ReadData(1, "Password"));
+            
             //pageEA.TextInitial.SendKeys("Test page navigation");
             //pageEA.TextFirstName.SendKeys("Bunny");
             //pageEA.TextMiddleName.SendKeys("Smith");
             //pageEA.ButtonSave.Click();
 
             //call the user fill form operation from the EAPageObject 
-            pageEA.UserFormFill("call userFormFill method from EAPageObject", "Bunny", "Smith");
+            //pageEA.UserFormFill("test", "Bunny", "Smith");
+            pageEA.UserFormFill(ExcelLib.ReadData(1, "Initial"), ExcelLib.ReadData(1, "FirstName"), ExcelLib.ReadData(1, "MiddleName"));
         }
 
         [TearDown]
