d6af77a9ac4c0e3fb18676420bd732a4b6e7bde2
Qian Zhou <qianfinland@gmail.com>
2015-08-10 13:51:54 +0300

    use of selenium server

--- WebDriverDemo/WebDriverDemo/BasicTests.cs
+++ WebDriverDemo/WebDriverDemo/BasicTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
@@ -13,7 +14,49 @@ namespace WebDriverDemo
     class BasicTests
     {
         IWebDriver driver = new ChromeDriver(@"C:\Users\Qian.Zhou\Documents\");
+        
 
+        [Test]
+        public void SearchGoogleImageTest()
+        {
+            driver.Url = "http://www.google.com";
+            //driver.Navigate().GoToUrl("http://www.google.com");//same as above
+ 
+            var searchBox = driver.FindElement(By.Id("lst-ib"));
+            searchBox.SendKeys("Pluralsight");
+            searchBox.SendKeys(Keys.Enter); // Use the enter key 
+
+
+            //clcik the image
+            //driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15)); // wait page to load fully and solve error on no such element 
+            //method 1 to find the image text 
+            //var imagetext = driver.FindElement(By.LinkText("Kuvahaku"));
+
+            //method 2 to find the image text
+            //var imagetext = driver.FindElement(By.Id("hdtb-msb")).FindElements(By.CssSelector("[class='hdtb-mitem hdtb-imb'"))[0].FindElement(By.TagName("a"));
+
+
+            //second method to wait elements to load 
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            var imagetext = wait.Until(d =>
+            {
+                var elements = driver.FindElement(By.Id("hdtb-msb")).FindElements(By.CssSelector("[class='hdtb-mitem hdtb-imb'"));
+                if (elements.Count() > 0)
+                    return elements[0].FindElement(By.TagName("a"));
+                return null;
+            });
+            Assert.That(imagetext.Text, Is.EqualTo("Kuvahaku"));
+            imagetext.Click();
+
+            //search a specific image, for example the 4th one 
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10)); //avoid error on no such element
+            var imagelistId = driver.FindElement(By.Id("rg_s"));
+            var fouthdivclass = imagelistId.FindElements(By.CssSelector("[class='rg_di rg_el ivg-i']"))[3];
+            fouthdivclass.Click();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            var ImageLink = driver.FindElement(By.LinkText("Näytä kuva"));
+            ImageLink.Click();
+        }
 
         [Test]
         public void DifferentElementsTest()
--- WebDriverDemo/WebDriverDemo/Program.cs
+++ WebDriverDemo/WebDriverDemo/Program.cs
@@ -15,58 +15,37 @@ namespace WebDriverDemo
 {
     class Program
     {
-        IWebDriver driver = new ChromeDriver(@"C:\Users\Qian.Zhou\Documents\");
+        
         static void Main(string[] args)
         {
-            //case of Firefox
-            //IWebDriver driver = new FirefoxDriver();
-            //driver.Url = "www.google.com";
 
-            ////case of IE
-            //IWebDriver driver1 = new InternetExplorerDriver(@"C:\Users\Qian.Zhou\Documents\");
-            //driver1.Url = "www.facebook.com";
-            //case for Chrome
-        }
-        [Test]
-        public void ImageSearchTest()
-        {
             
+        }  
+   
+
+
+        [Test]
+        public void Selenium_Server_Test()
+        {
+            IWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), DesiredCapabilities.Firefox());
             driver.Url = "http://www.google.com";
-            //driver.Navigate().GoToUrl("http://www.google.com");//same as above 
             var searchBox = driver.FindElement(By.Id("lst-ib"));
-            searchBox.SendKeys("Pluralsight");
-            searchBox.SendKeys(Keys.Enter); // Use the enter key 
+            searchBox.SendKeys("Lotus flower");
+            searchBox.SendKeys(Keys.Enter);
 
-
-            //clcik the image
-            /*driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10)); // wait page to load fully and solve error on no such element 
-            //method 1 to find the image text 
-            //var imagetext = driver.FindElement(By.LinkText("Kuvahaku"));
-
-            //method 2 to find the image text
-            var imagetext = driver.FindElement(By.Id("hdtb-msb")).FindElements(By.CssSelector("[class='hdtb-mitem hdtb-imb'"))[0].FindElement(By.TagName("a"));
-            */
-
-            //second method to wait elements to load 
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            var imagetext = wait.Until(d =>
-                {
-                    var elements = driver.FindElement(By.Id("hdtb-msb")).FindElements(By.CssSelector("[class='hdtb-mitem hdtb-imb'"));
-                    if (elements.Count() > 0)
-                        return elements[0].FindElement(By.TagName("a"));
-                    return null;
-                });
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            var imagetext = driver.FindElement(By.LinkText("Kuvahaku"));
             Assert.That(imagetext.Text, Is.EqualTo("Kuvahaku"));
             imagetext.Click();
 
-            //search a specific image, for example the 4th one 
             driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10)); //avoid error on no such element
             var imagelistId = driver.FindElement(By.Id("rg_s"));
-            var fouthdivclass = imagelistId.FindElements(By.CssSelector("[class='rg_di rg_el ivg-i']"))[3];
+            var fouthdivclass = imagelistId.FindElements(By.CssSelector("[class='rg_di rg_el ivg-i']"))[1].FindElement(By.TagName("a"));
             fouthdivclass.Click();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
             var ImageLink = driver.FindElement(By.LinkText("Näytä kuva"));
             ImageLink.Click();
-        }      
+        }
 
     }
 }
